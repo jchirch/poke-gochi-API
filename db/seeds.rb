79258c49c3944184d7dda597f9b90d5dc6c9b035
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-10-31 15:21:16 -0600

    Feat: seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,101 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(
+Trainer.create!(
     name: "Ash"
 )
 
 Pokemon.create!(
-    
+    name:"gastly",
+    description: "spooky boi",
+    img_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/92.gif",
+    cry_url: "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/92.ogg",
+    small_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/92.png",
+    level: 1,
+    xp: 0,
+    energy: 110,
+    max_energy: 110,
+    happiness: 100,
+    user_id: 1
 )
+
+Pokemon.create!(
+    name:"charmander",
+    description: "hot lil doggy",
+    img_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/4.gif",
+    cry_url: "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/4.ogg",
+    small_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/4.png",
+    level: 1,
+    xp: 0,
+    energy: 110,
+    max_energy: 110,
+    happiness: 100,
+    user_id: 1
+)
+
+Pokemon.create!(
+    name: "Magikarp",
+    description: "In the distant past, it was somewhat stronger than the horribly weak descendants that exist today.",
+    img_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/129.gif",
+    cry_url: "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/129.ogg",
+    small_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/129.png",
+    level: 1,
+    xp: 0,
+    energy: 110,
+    max_energy: 110,
+    happiness: 100,
+    user_id: 1
+)
+
+Pokemon.create!(
+    name: "Shinx",
+    description: "This Pokémon generates electricity by contracting\nits muscles. Excited trembling is a sign that Shinx\nis generating a tremendous amount of electricity.",
+    img_url: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/403.gif",
+    cry_url: "https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/403.ogg",
+    small_img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/403.png",
+    level: 5,
+    xp:0,
+    energy: 100,
+    max_energy: 150,
+    happiness: 80,
+    user_id: 1
+)
+
+# Pokemon.create!(
+#     name:"gastly",
+#     description: "",
+#     img_url: "",
+#     cry_url: "",
+#     small_img: "",
+#     level: 0,
+#     xp: 0,
+#     energy: 0,
+#     max_energy: 0,
+#     happiness: 0
+# )
+
+# Pokemon.create!(
+#     name:"gastly",
+#     description: "",
+#     img_url: "",
+#     cry_url: "",
+#     small_img: "",
+#     level: 0,
+#     xp: 0,
+#     energy: 0,
+#     max_energy: 0,
+#     happiness: 0
+# )
+
+# Pokemon.create!(
+#     name:"gastly",
+#     description: "",
+#     img_url: "",
+#     cry_url: "",
+#     small_img: "",
+#     level: 0,
+#     xp: 0,
+#     energy: 0,
+#     max_energy: 0,
+#     happiness: 0
+# )
