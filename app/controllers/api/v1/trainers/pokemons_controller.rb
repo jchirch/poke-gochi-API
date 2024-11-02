b36831f30bae5076b46ac5f9cadc62312a193480
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-11-01 21:51:11 -0400

    merged main into feat/pokeapiservice

--- app/controllers/api/v1/trainers/pokemons_controller.rb
+++ app/controllers/api/v1/trainers/pokemons_controller.rb
@@ -1,3 +1,15 @@
 class Api::V1::PokemonsController < ApplicationController
+  def show
+    begin
+      pokemon_name = params[:name] 
+      species_data = PokeApiService.get_pokemon_species(pokemon_name)
+      details_data = PokeApiService.get_pokemon_details(pokemon_name)
+      require 'pry'; binding.pry
 
+      pokemon = PokemonsSerializer.populate_attributes(details_data)
+      render json: PokemonsSerializer.new(pokemon)
+    rescue StandardError => e
+      render json: { error: e.message }, status: :bad_request
+    end
+  end
 end
--- spec/requests/api/v1/trainer/pokemons_request_spec.rb
+++ spec/requests/api/v1/trainer/pokemons_request_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Pokemon Endpoints' do
-  
+  describe "PokeApiService" do 
+    it 'returns pokemon details' do 
+      response = PokeApiService.get_pokemon_details('pikachu')
+      require 'pry'; binding.pry
+    end
+  end
 end
