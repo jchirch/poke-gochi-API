cd1d795997495e83922e80db7c04393be468cf6b
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-11-02 19:23:54 -0400

    Created create action for Pokemons post

--- app/controllers/api/v1/trainers/pokemons_controller.rb
+++ app/controllers/api/v1/trainers/pokemons_controller.rb
@@ -1,15 +1,16 @@
-class Api::V1::PokemonsController < ApplicationController
-  def show
-    begin
-      pokemon_name = params[:name] 
-      species_data = PokeApiService.get_pokemon_species(pokemon_name)
-      details_data = PokeApiService.get_pokemon_details(pokemon_name)
-      require 'pry'; binding.pry
-
-      pokemon = PokemonsSerializer.populate_attributes(details_data)
-      render json: PokemonsSerializer.new(pokemon)
-    rescue StandardError => e
-      render json: { error: e.message }, status: :bad_request
-    end
+class Api::V1::Trainers::PokemonsController < ApplicationController
+  def create
+    pokemon_name = pokemon_params[:name]
+    trainer_id = params[:trainer_id]
+    pokemon = Pokemon.populate_with_api_data(pokemon_params, pokemon_name, trainer_id)
+    pokemon.save!
+    render json: PokemonSerializer.new(pokemon)
   end
+
+  private 
+
+  def pokemon_params
+    params.permit(:name, :level, :xp, :energy, :max_energy, :happiness, :trainer_id)
+  end
+
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,25 @@
 class Pokemon < ApplicationRecord
     belongs_to :trainer
+
+  def self.populate_with_api_data(pokemon_params, pokemon_name, trainer_id)
+    species_data = PokeApiService.get_pokemon_species(pokemon_name)
+    details_data = PokeApiService.get_pokemon_details(pokemon_name)
+    Pokemon.new(
+      name: pokemon_params[:name],
+      level: pokemon_params[:level],
+      xp: pokemon_params[:xp],
+      energy: pokemon_params[:energy],
+      max_energy: pokemon_params[:max_energy],
+      happiness: pokemon_params[:happiness],
+      description: clean_description(species_data[:flavor_text_entries][0][:flavor_text]),
+      gif_url: details_data[:sprites][:other][:showdown][:front_shiny],
+      cry_url: details_data[:cries][:latest],
+      small_img: details_data[:sprites][:front_default],
+      trainer_id: trainer_id
+    )
+  end 
+
+  def self.clean_description(description)
+   description.gsub("\n", ' ').strip
+  end 
 end
