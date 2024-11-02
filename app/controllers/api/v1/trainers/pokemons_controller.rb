995fd36eabe2da6c4115f608508c09a341704884
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-11-02 16:18:22 -0600

    Test: pokemon req index test

--- app/controllers/api/v1/trainers/pokemons_controller.rb
+++ app/controllers/api/v1/trainers/pokemons_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::PokemonsController < ApplicationController
 
+  def index
+    pokemons = Pokemon.all
+    
+  end
+
 end
--- spec/requests/api/v1/trainer/pokemons_request_spec.rb
+++ spec/requests/api/v1/trainer/pokemons_request_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe 'Pokemon Endpoints' do
+  before (:each) do
+    @trainer1 = Trainer.create!(name: "Ash")
+    @pikachu = Pokemon.create!(name: "Pikachu", 
+                              description: "yellow rat", 
+                              gif_url: "example.com", 
+                              cry_url:  "example.com", 
+                              small_img:  "example.com",
+                              level: 1,
+                              xp: 1,
+                              energy: 1,
+                              max_energy: 1,
+                              happiness: 1,
+                              trainer_id: @trainer1.id)
+    @mew = Pokemon.create!(name: "Mew", 
+                              description: "pink rat", 
+                              gif_url: "example.com", 
+                              cry_url:  "example.com", 
+                              small_img:  "example.com",
+                              level: 1,
+                              xp: 1,
+                              energy: 1,
+                              max_energy: 1,
+                              happiness: 1,
+                              trainer_id: @trainer1.id)                             
+  end
+
+  describe "Happy Paths" do
+    it "Returns all of a trainer's pokemon #index" do
+      get "/api/v1/trainers/#{@trainer1.id}/pokemons"
+      expect(response).to be_successful
+      require 'pry'; binding.pry
+      pokemons = JSON.parse(response.body, symbolize_names: true)
+
+
+    end
+
+  end
   
 end
