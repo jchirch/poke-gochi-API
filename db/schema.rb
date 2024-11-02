5e362a9463e6828118d4701a3292c98816b0b9a4
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-11-02 19:24:58 -0400

    Added index to name and id for pokemons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_01_154136) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_02_232405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_01_154136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "trainer_id", null: false
+    t.index ["id"], name: "index_pokemons_on_id"
+    t.index ["name"], name: "index_pokemons_on_name"
     t.index ["trainer_id"], name: "index_pokemons_on_trainer_id"
   end
 
