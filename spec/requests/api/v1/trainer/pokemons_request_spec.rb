e8eb0945b1f78514edd6074f27b29311feb3a52d
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-11-02 18:38:42 -0600

    Test: fill out happy and sad paths for index action

--- spec/requests/api/v1/trainer/pokemons_request_spec.rb
+++ spec/requests/api/v1/trainer/pokemons_request_spec.rb
@@ -30,21 +30,63 @@ RSpec.describe 'Pokemon Endpoints' do
   describe "Happy Paths" do
     it "Returns all of a trainer's pokemon #index" do
       get "/api/v1/trainers/#{@trainer1.id}/pokemons"
-      #  require 'pry'; binding.pry
+
       expect(response).to be_successful
-      pokemons = JSON.parse(response.body, symbolize_names: true)
+      pokemons = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      pokemons.each do |pokemon|
+        expect(pokemon).to have_key(:id)
+        expect(pokemon[:id]).to be_a(String)
+
+        expect(pokemon).to have_key(:type)
+        expect(pokemon[:type]).to eq("pokemon")
+
+        attributes = pokemon[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+
+        expect(attributes).to have_key(:description)
+        expect(attributes[:description]).to be_a(String)
+
+        expect(attributes).to have_key(:gif_url)
+        expect(attributes[:gif_url]).to be_a(String)
+
+        expect(attributes).to have_key(:cry_url)
+        expect(attributes[:cry_url]).to be_a(String)
+
+        expect(attributes).to have_key(:small_img)
+        expect(attributes[:small_img]).to be_a(String)
+
+        expect(attributes).to have_key(:level)
+        expect(attributes[:level]).to be_a(Integer)
+
+        expect(attributes).to have_key(:xp)
+        expect(attributes[:xp]).to be_a(Integer)
+
+        expect(attributes).to have_key(:energy)
+        expect(attributes[:energy]).to be_a(Integer)
+
+        expect(attributes).to have_key(:max_energy)
+        expect(attributes[:max_energy]).to be_a(Integer)
+
+        expect(attributes).to have_key(:happiness)
+        expect(attributes[:happiness]).to be_a(Integer)
+
+        expect(attributes).to have_key(:trainer_id)
+        expect(attributes[:trainer_id]).to eq(@trainer1.id)
+      end
     end
   end
 
   describe "Sad Paths" do
     it "Returns error with invalid request" do
-      get "/api/v1/trainers/0"
+      get "/api/v1/trainers/0/pokemons"
       expect(response).to_not be_successful
       expect(response.status).to eq(404)
 
       error = JSON.parse(response.body, symbolize_names: true)
       expect(error[:message]).to eq("404, result not found")
-      expect(error[:errors]).to eq(["Couldn't find Trainer with 'id'=0"])
     end
   end
 end
