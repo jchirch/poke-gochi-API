dd7251e259db06915ec46b3338bd74839f96bbfc
Kyle Delaney <khdelaney99@gmail.com>
2024-11-04 07:53:36 -0500

    add job files

---
+++ app/jobs/decrement_energy_job.rb
@@ -0,0 +1,7 @@
+class DecrementEnergyJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ app/jobs/decrement_happiness_job.rb
@@ -0,0 +1,7 @@
+class DecrementHappinessJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
