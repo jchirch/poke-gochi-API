9124436edbfbc3309e288681542ab3024854fac2
Kyle Delaney <khdelaney99@gmail.com>
2024-11-05 19:13:14 -0500

    add redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'poke-gotchi-api' }
+  config.redis = { url: ENV['REDISCLOUD_URL'] }
 end
   
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDISCLOUD_URL'], namespace: 'poke-gotchi-api' } 
+  config.redis = { url: ENV['REDISCLOUD_URL'] } 
 end
   
